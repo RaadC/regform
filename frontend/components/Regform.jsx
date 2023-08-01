faa38d2ece1bdbdfe266a5475b137b097ff851b8
RaadC <jannradley.maniago@gsfe.tupcavite.edu.ph>
2023-08-01 13:47:22 +0800

    regform

--- frontend/components/Regform.jsx
+++ frontend/components/Regform.jsx
@@ -3,40 +3,38 @@ import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import styles from "./Regform.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Form() {
   const RegisterSchema = yup.object().shape({
     firstname: yup
       .string()
       .label("First Name")
-      .required("*enter first name")
-      .min(2)
-      .max(20),
+      .required("*")
+      .min(2, "* too short")
+      .max(20, "* too long"),
     lastname: yup
       .string()
       .label("Last Name")
-      .required("*enter last name")
-      .min(2)
-      .max(20),
+      .required("*")
+      .min(2, "* too short")
+      .max(20, "* too short"),
     email: yup
       .string()
       .label("Email Address")
-      .required("*enter email")
-      .min(10)
-      .max(150)
-      .email("enter a correct email"),
-    address: yup
-      .string()
-      .label("Address")
-      .required("*enter address"),
+      .required("*")
+      .min(10, "* too short")
+      .max(150, "* too long")
+      .email("*incorrect"),
+    address: yup.string().label("Address").required("*"),
     mobile: yup
       .string()
       .label("Mobile")
-      .required("*enter mobile number")
-      .min(11, "*enter correct mobile number")
-      .max(11, "*enter correct mobile number"),
-    status: yup.string().label("TUP Student").required("*enter yes or no"),
-    message: yup.string().label("Message").required("*enter message"),
+      .required("*")
+      .min(11, "* too short")
+      .max(11, "* too long"),
+    status: yup.string().label("TUP Student").required("*"),
+    message: yup.string().label("Message").required("*"),
   });
 
   const {
@@ -71,93 +69,108 @@ export default function Form() {
 
   return (
     <div className={styles.container}>
-      <img className= {styles.img} src="tuplogo.png" />
-      <form onSubmit={handleSubmit((data) => submitForm(data))}>
-        <h1>TUP Registration Form</h1>
-        <div className={styles.row1}>
-          <div className={styles.inputcontainerLeft}>
-            <input className={styles.holder}
+      <div className={styles.headercontainer}>
+        <img src="tuplogo.png" />
+        <h1>TUPC Registration Form</h1>
+      </div>
+      <form
+        className={styles.regform}
+        onSubmit={handleSubmit((data) => submitForm(data))}
+      >
+        <div className={"form-group row mb-30"}>
+          <div className={"form-group col"}>
+            <label for="firstName">
+              First Name <span>{errors.firstname?.message}</span>
+            </label>
+            <input
+              className={"form-control form-control-lg"}
+              id="firstName"
               type="text"
-              placeholder="First Name"
               {...register("firstname", { required: true })}
             />
-            <span>{errors.firstname?.message}</span>
           </div>
-          <div className={styles.inputcontainerRight}>
-            <input className={styles.holder}
+          <div className={"form-group col"}>
+            <label for="lastName">
+              Last Name <span>{errors.lastname?.message}</span>
+            </label>
+            <input
+              className={"form-control form-control-lg"}
+              id="lastName"
               type="text"
-              placeholder="Last Name"
               {...register("lastname", { required: true })}
             />
-            <span>{errors.lastname?.message}</span>
           </div>
         </div>
-        <div className={styles.row2}>
-          <div className={styles.row2group}>
-            <div className={styles.inputcontainerLeft}>
-              <input className={styles.holder}
-                type="text"
-                placeholder="Email Address"
-                {...register("email", { required: true })}
-              />
-              <span>{errors.email?.message}</span>
-            </div>
-            <div className={styles.inputcontainerLeft}>
-              <input className={styles.holder}
-                type="text"
-                placeholder="Mobile Number"
-                {...register("mobile", { required: true, maxLength: 10 })}
-              />
-              <span>{errors.mobile?.message}</span>
-            </div>
-          </div>
-          <div className={styles.inputcontainerRight}>
-            <textarea
-              rows={4}
-              cols={20}
-              type="text"
-              placeholder="Address"
-              {...register("address", { required: true })}
-            />
-            <span>{errors.address?.message}</span>
-          </div>
+        <div className={"form-group"}>
+          <label for="email">
+            Email Address <span>{errors.email?.message}</span>
+          </label>
+          <input
+            className={"form-control form-control-lg"}
+            id="email"
+            type="text"
+            {...register("email", { required: true })}
+          />
         </div>
-        <div className={styles.row3}>
-          <div className={styles.inputcontainerLeft}>
-            <textarea
-              rows={4}
-              cols={20}
-              type="text"
-              placeholder="Why do you want to study here?"
-              {...register("message", { required: true })}
-            />
-            <span>{errors.message?.message}</span>
-          </div>
-          <div className={styles.row3group}>
-            <div className={styles.inputcontainerRight}>
-              <select className= {styles.old}
-                defaultValue=""
-                {...register("status")}
-                onChange={(e) => SelectValidator(e.target.value)}
-              >
-                <option value="" disabled>
-                  Old TUP Student?
-                </option>
-                <option value="1">Yes</option>
-                <option value="0">No</option>
-              </select>
-              <span>{errors.status?.message}</span>
-            </div>
-            <div className={styles.inputcontainerRight}>
-              <input
-                className={styles.submitBtn}
-                type="submit"
-                name="submit"
-                values="Submit"
-              />
-            </div>
-          </div>
+        <div className={"form-group"}>
+          <label for="mobile">
+            Mobile # <span>{errors.mobile?.message}</span>
+          </label>
+          <input
+            className={"form-control form-control-lg"}
+            id="mobile"
+            type="text"
+            {...register("mobile", { required: true, maxLength: 10 })}
+          />
         </div>
+        <div className={"form-group"}>
+          <label for="address">
+            Address <span>{errors.address?.message}</span>
+          </label>
+          <input
+            className={"form-control form-control-lg"}
+            id="address"
+            type="text"
+            {...register("address", { required: true })}
+          />
+        </div>
+        <div className={"form-group"}>
+          <label for="message">
+            Message <span>{errors.message?.message}</span>
+          </label>
+          <textarea
+            class="form-control form-control-lg"
+            id="message"
+            rows={4}
+            cols={20}
+            type="text"
+            {...register("message", { required: true })}
+          />
+        </div>
+        <div className={"form-group"}>
+          <label for="selectOption">
+            Old TUPC Student? <span>{errors.status?.message}</span>
+          </label>
+          <select
+            className={"form-control form-control-lg w-50"}
+            id="selectOption"
+            defaultValue=""
+            {...register("status")}
+            onChange={(e) => SelectValidator(e.target.value)}
+          >
+            <option value="" disabled>
+              Old TUP Student?
+            </option>
+            <option value="1">Yes</option>
+            <option value="0">No</option>
+          </select>
+        </div>
+        <input
+          className={"btn btn-primary btn-lg w-50"}
+          type="submit"
+          name="submit"
+          values="Submit"
+        />
       </form>
     </div>
   );
